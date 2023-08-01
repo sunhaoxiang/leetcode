af6fe247ab287498204d92bfe4e88099a12568c3
sunhx <673686754@qq.com>
2023-08-02 07:47:40 +0800

    657. Robot Return to Origin

--- 1.TwoSum.js
+++ 1.TwoSum.js
@@ -4,7 +4,6 @@
 // and you may not use the same element twice.
 // You can return the answer in any order.
 
-
 const twoSum = function(nums, target) {
   const prevNums = {}
   for (let i = 0; i < nums.length; i++) {
